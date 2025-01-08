3dc8d84ff186281423e6228227d6b3b45248dbad
whitebeard19 <basith2309004@gmail.com>
2025-01-08 21:20:22 +0530

    created base file

--- apps/clubs/models.py
+++ apps/clubs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Club(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    logo = models.ImageField(upload_to='club_logo/')
+    email = models.EmailField()
+    admin_password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
